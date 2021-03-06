1f25e61892bd949dc407ab0884d4ca4424b6bc88
Luis Ariel Vega Soliz <vsoliz.ariel@gmail.com>
2021-03-06 01:51:10 -0400

    corregidos receta del webserver

--- cookbooks/webserver/recipes/default.rb
+++ cookbooks/webserver/recipes/default.rb
@@ -9,13 +9,13 @@ dbserver_name = node['webserver']['dbserver_name']
 ruby_block 'edit etc hosts' do
     block do
         fe = Chef::Util::FileEdit.new('/etc/hosts')
-        fe.insert_line_if_no_match(/\W#{Regexp.escape(dbserver_name)}\W/, '#{dbserver_ip}    #{dbserver_name}')
+        fe.insert_line_if_no_match(/\W#{Regexp.escape(dbserver_name)}\W/, "#{dbserver_ip}    #{dbserver_name}")
         fe.write_file
     end
 end
 
 execute 'install nginx' do
-    command 'sudo dnf update && sudo dnf install -y nginx'
+    command 'sudo dnf update -y && sudo dnf install -y nginx'
     action :run
 end
 
